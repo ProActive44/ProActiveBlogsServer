37a9288f0a424a5f0de63f9487188d73a75bdc50
Prasad <prasadmhaske2001@gmail.com>
2023-08-06 17:16:55 +0530

    basic server created

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const cors = require("cors");
+const connection = require("./config/db");
+const signupRouter = require("./routes/signup.Router");
+const loginRouter = require("./routes/login.Router");
+require("dotenv").config();
+
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+
+app.get("/", (req, res) => {
+  res.send("hello");
+});
+
+app.use("/signup", signupRouter);
+app.use("/login", loginRouter);
+
+app.listen(process.env.PORT, async () => {
+  try {
+    await connection;
+    console.log(`Server has started on ${process.env.PORT}`);
+  } catch (error) {
+    console.log(error);
+  }
+});
---
+++ models/user.model.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require("mongoose");
+
+const userSchema = new Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+const userModel = model("user", userSchema);
+
+module.exports = userModel;
